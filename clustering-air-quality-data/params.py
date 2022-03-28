3d820567878603880ff2a416ae4cfe92faab6863
David Liu <shiqian_liu@hotmail.com>
2022-03-28 14:50:11 -0400

    read data from bucket

---
+++ clustering-air-quality-data/params.py
@@ -0,0 +1,5 @@
+# project id
+PROJECT_ID='clustering-air-quality-data'
+
+# bucket name
+BUCKET_NAME='bucket-of-david'
---
+++ clustering-air-quality-data/pipeline.py
@@ -0,0 +1,43 @@
+from params import BUCKET_NAME, PROJECT_ID
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam import Pipeline
+from apache_beam.io.textio import ReadFromText, WriteToText
+from apache_beam.options.pipeline_options import SetupOptions
+import argparse
+
+# os.environ[‘GOOGLE_APPLICATION_CREDENTIALS’] = “/home/annapooraniks/gcpdataflow/gcpdataflowkey.json”
+
+class MyOptions(PipelineOptions):
+
+  @classmethod
+  def _add_argparse_args(cls, parser):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--runner', default='dataflow')
+    parser.add_argument('--project', default=PROJECT_ID)
+    parser.add_argument('--temp_location', default=f"gs://{BUCKET_NAME}/temp")
+    parser.add_argument('--staging_location', default=f"gs://{BUCKET_NAME}/staging")
+    parser.add_argument('--region', default='us-central1')
+
+def get_pipeline():
+    pipeline_options = PipelineOptions().view_as(MyOptions)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+    p = Pipeline(options=pipeline_options)
+    return p
+
+if __name__ == "__main__":
+    p = get_pipeline()
+    small_sum = (
+           p 
+           | 'Read data' >> ReadFromText(f'gs://{BUCKET_NAME}/*.csv')
+           | 'Write data' >> WriteToText("raw_data/test.csv")
+          )
+    result = p.run()
+    result.wait_until_finish()
+    
+    
+    
+
+
+      
+      
+   
