fc4a20a5eaff6ea0a42395f4c9f2ac45a4877e89
David Liu <shiqian_liu@hotmail.com>
2022-03-30 17:09:37 -0400

    remove api key

--- clustering-air-quality-data/pub_sub/publisher.py
+++ clustering-air-quality-data/pub_sub/publisher.py
@@ -1,5 +1,7 @@
 from google.cloud import pubsub_v1
 from params import PROJECT_ID
+from pub_sub.data import get_AQ_data
+import time
 
 if __name__ == "__main__":
 
@@ -7,10 +9,17 @@ if __name__ == "__main__":
     project = PROJECT_ID
 
     # Replace  with your pubsub topic
-    pubsub_topic = ''  
+    pubsub_topic = f'projects/{PROJECT_ID}/topics/AQ_Topic'  
 
     # create publisher
     publisher = pubsub_v1.PublisherClient()
     
     
+    data = get_AQ_data()
     
+     # loop over each record
+    for record in data:
+        event_data = record   # entire line of input CSV is the message
+        print('Publishing {0} to {1}'.format(event_data, pubsub_topic))
+      #   publisher.publish(pubsub_topic, event_data)
+        time.sleep(1) 
